ea7b3d623d26da2e6b3d2d58a341d46109a232b5
Kevin Li <li.kevin.kw@gmail.com>
2021-06-11 21:26:49 -0400

    (only comments) added comments to special deck file for clarity

--- src/SpecialDeck.js
+++ src/SpecialDeck.js
@@ -1,2 +1,15 @@
-var specialdeck = ["!Y", "!B", "!P", "!G", "!H", "!C", "!~", "!K", "!T", "!X", "!L", "!U"];
+var specialdeck = [
+    "!Y", // breakaway
+    "!B", // bodyguard
+    "!P", // paradigm shift
+    "!G", // gravedigger
+    "!H", // hand of god
+    "!C", // colorblind
+    "!~", // sleight of hand
+    "!K", // act of kindness
+    "!T", // redirect
+    "!X", // duel
+    "!L", // lifeline
+    "!U"  // unseeing eye
+];
 export default specialdeck;
